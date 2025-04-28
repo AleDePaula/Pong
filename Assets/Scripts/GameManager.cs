dd04104c987de9305b89f58619d02d1b83c68a56
Alexandre de Paula <alexandreaugusto@gmail.com>
2025-04-28 10:36:36 -0300

    First Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    public int playerScore;
+    public int enemyScore;
+    public float ballSpeed;
+    
+}
