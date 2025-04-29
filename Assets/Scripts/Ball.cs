c996a7b66585d410f5883a8d6835009467d7e1aa
Alexandre de Paula <alexandreaugusto@gmail.com>
2025-04-29 16:37:26 -0300

    [feat]-Ball colliding,scoring and reseting

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Collider2D col;
+    private Vector2 ballDirection;
+    private float ballSpeed;
+    private Vector2 ballInitialPosition;
+    private GameManager gameManager;
+    private void Awake()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        ballSpeed = gameManager.initialBallSpeed;
+        ResetBall();     
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name=="LeftBorder" || collision.gameObject.name=="RightBorder")
+        {
+            gameManager.Score(collision.gameObject.name);
+            ResetBall();
+        }
+        else if(collision.gameObject.name=="TopBorder" || collision.gameObject.name=="BottomBorder")
+        {
+            ballDirection.y = -1*ballDirection.y;
+            
+        }
+        else if(collision.gameObject.name=="PlayerPaddle" || collision.gameObject.name=="EnemyPaddle")        {
+            
+            ballDirection.x = -1*ballDirection.x;
+            ballSpeed += 0.5f;
+            if(ballSpeed > gameManager.maxBallSpeed)
+            {
+                ballSpeed = gameManager.maxBallSpeed;
+            }
+        }
+        
+
+    }
+    private void ResetBall()
+    {
+        this.transform.position = ballInitialPosition;
+        ballSpeed = gameManager.initialBallSpeed;
+        
+        ballDirection = GetDirection();
+       
+        rb.linearVelocity = ballDirection * ballSpeed;
+    }
+
+    void Update()
+    {
+       rb.linearVelocity = ballDirection * ballSpeed;
+    }
+
+    private Vector2 GetDirection()
+    {
+        bool right = Random.value < 0.5f;
+        float angle;
+
+        if (right){
+            if(Random.value > 0.5f)
+                angle = Random.Range(30f, 60f);
+            else
+                angle = Random.Range(30f, 60f);
+        }
+        else
+        {
+            if(Random.value > 0.5f)
+                angle = Random.Range(120f, 150f);
+            else
+                angle = Random.Range(210f, 240f);
+            
+        }
+        return new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad)).normalized;
+    }
+
+
+
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,16 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class GameManager : MonoBehaviour
 {
     
     public static GameManager instance;
+    public int playerScore;
+    public int enemyScore;
+    public float initialBallSpeed;   
+    public float maxBallSpeed;
     private void Awake()
     {
         if (instance == null)
@@ -16,9 +22,27 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    
-    public int playerScore;
-    public int enemyScore;
-    public float ballSpeed;
+
+    private void Start()
+    {
+        initialBallSpeed = 10f;
+        maxBallSpeed = 20f;
+        playerScore = 0;
+        enemyScore = 0;
+    }
+
+    public void Score(String sideScored)
+    {
+        if (sideScored == "LeftBorder")
+        {
+            enemyScore++;
+            print(enemyScore);
+        }
+        else if (sideScored == "RightBorder")
+        {
+            playerScore++;
+            print(playerScore);
+        }
+    }    
     
 }
